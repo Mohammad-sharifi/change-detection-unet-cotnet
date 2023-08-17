5a0f6cfbf0ef1162ea2d2112b5d641280a63ce40
vahidmir2023 <142111858+vahidmir2023@users.noreply.github.com>
2023-08-17 18:14:15 +0330

    Update dataset.py

--- change_detection/dataset.py
+++ change_detection/dataset.py
@@ -271,7 +271,8 @@ class CD2DDataset_2(torch.utils.data.Dataset):
         label = imageio.v3.imread(self.imgs_path["2D"][index]) / 255.
         label = torch.from_numpy(label)
         label = torch.concat([1-label, label], axis=0)
-    
+        img1 = torch.from_numpy(img1)
+        img2 = torch.from_numpy(img2)
         img = torch.cat([img1, img2], axis=0)
         
         return img, label
