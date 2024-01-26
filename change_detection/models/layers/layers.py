8922aeff45656a08cff3eee3321f461b449f6894
m <mohammad1377sharifi@gmail.com>
2024-01-26 16:43:15 +0330

    Add encoder block

--- change_detection/models/layers/layers.py
+++ change_detection/models/layers/layers.py
@@ -3,17 +3,7 @@ import torch.nn as nn
 from torch import Tensor
 from .initializers import he_normalizer
 from models.cotnet import CotLayer
-from cupy_layers.aggregation_zeropad import LocalConvolution
-from models.layers import get_act_layer
-
-
-__all__ = [
-    "Conv2dWithHeNorm",
-    "CustomConv2d",
-    "ConvBlock",
-    "UpsamplingBlock",
-    "BackBone",
-]
+from einops import rearrange
 
 
 class ConvBlock(nn.Module):
@@ -153,3 +143,137 @@ class UpsamplingBlock(nn.Module):
             out = torch.cat([out, merge], axis=1)
         
         return self.conv_bn4(self.conv_bn3(out))
+
+
+class ChunkGate(nn.Module):
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1*x2
+
+
+class LayerNormFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps):
+        ctx.eps = eps
+        N, C, H, W = x.size()
+        mu = x.mean(1, keepdim=True)
+        var = (x - mu).pow(2).mean(1, keepdim=True)
+        y = (x - mu) / (var + eps).sqrt()
+        ctx.save_for_backward(y, var, weight)
+        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        eps = ctx.eps
+
+        N, C, H, W = grad_output.size()
+        y, var, weight = ctx.saved_variables
+        g = grad_output * weight.view(1, C, 1, 1)
+        mean_g = g.mean(dim=1, keepdim=True)
+
+        mean_gy = (g * y).mean(dim=1, keepdim=True)
+        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(
+            dim=0), None
+
+
+class LayerNorm2d(nn.Module):
+    def __init__(self, channels, eps=1e-6):
+        super(LayerNorm2d, self).__init__()
+        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
+        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
+        self.eps = eps
+
+    def forward(self, x):
+        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
+
+
+class EncoderSpaChAttBlock(nn.Module):
+    def __init__(self, in_channels, num_heads=3, chn_expanded=2, dropout_rate=0.0):
+        super(EncoderSpaChAttBlock, self).__init__()
+
+        self.num_heads = num_heads
+
+        self.qkv = nn.Conv2d(in_channels, in_channels*3, kernel_size=1)
+        self.qkv_dwconv = nn.Conv2d(in_channels*3, in_channels*3, kernel_size=3, padding=1, stride=1, groups=in_channels*3)
+
+        self.prj_outc = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+        self.tempc = nn.Parameter(torch.ones(num_heads, 1, 1))
+        self.prj_outs = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+        self.temps = nn.Parameter(torch.ones(num_heads, 1, 1))
+
+        self.gate = ChunkGate()
+
+        ffn_channel = chn_expanded * in_channels
+        self.conv4 = nn.Conv2d(in_channels=in_channels, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1,
+                               bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=in_channels, kernel_size=1, padding=0, stride=1,
+                               groups=1, bias=True)
+
+        self.norm1 = LayerNorm2d(in_channels)
+        self.norm2 = LayerNorm2d(in_channels)
+
+        self.dropout1 = nn.Dropout(dropout_rate) if dropout_rate > 0. else nn.Identity()
+        self.dropout2 = nn.Dropout(dropout_rate) if dropout_rate > 0. else nn.Identity()
+
+        self.betac = nn.Parameter(torch.zeros((1, in_channels, 1, 1)), requires_grad=True)
+        self.betas = nn.Parameter(torch.zeros((1, in_channels, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, in_channels, 1, 1)), requires_grad=True)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, inp):
+        x = inp
+        x = self.norm1(x)
+        b, c, h, w = x.shape
+        qkv = self.qkv_dwconv(self.qkv(x))
+        q, k, v = qkv.chunk(3, dim=1)
+
+        q = rearrange(q, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        k = rearrange(k, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        v = rearrange(v, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+
+        
+        qs = q.clone().permute(0, 1, 3, 2)
+        ks = k.clone().permute(0, 1, 3, 2)
+        vs = v.clone().permute(0, 1, 3, 2)
+
+        q = torch.nn.functional.normalize(q, dim=-1)
+        k = torch.nn.functional.normalize(k, dim=-1)
+
+        # # Channel attention
+        attn = (q @ k.transpose(-2, -1)) * self.tempc
+        attn = self.relu(attn)
+        attn = self.softmax(attn)
+
+        outc = (attn @ v)
+
+        # Spatial Attention
+        qs = torch.nn.functional.normalize(qs, dim=-1)
+        ks = torch.nn.functional.normalize(ks, dim=-1)
+
+        attns = (qs @ ks.transpose(-2, -1)) * self.temps
+        attns = self.relu(attns)
+        attns = self.softmax(attns)
+        outs = (attns @ vs)
+        outs = outs.permute(0, 1, 3, 2)
+
+        outc = rearrange(outc, 'b head c (h w) -> b (head c) h w', head=self.num_heads, h=h, w=w)
+        outs = rearrange(outs, 'b head c (h w) -> b (head c) h w', head=self.num_heads, h=h, w=w)
+
+        xc = self.prj_outc(outc)
+        xc = self.dropout1(xc)
+        xs = self.prj_outs(outs)
+        xs = self.dropout1(xs)
+
+        y = inp + xc * self.betac + xs * self.betas
+
+        x = self.conv4(self.norm2(y))
+        x = self.gate(x)
+        x = self.conv5(x)
+
+        x = self.dropout2(x)
+
+        return y + x * self.gamma
+
